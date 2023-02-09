b87f80b21612e85060d23d28552ebcd2e2a46069
diegogirao <diegoramiro25@gmail.com>
2023-02-09 20:13:15 -0300

    implementacao e estilizacao do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,32 @@
 import styles from "./Header.module.css"
 
+import logoUrl from "../assets/images/logo.svg"
+
 export function Header() {
 	return (
 		<header className={styles.header}>
-			<h1>url shortening</h1>
+			<nav className="nav-list">
+				<a className="logo" href="/">
+					<img src={logoUrl} alt="logo do Shortly" />
+				</a>
+				<ul className="nav-list-item">
+					<li>
+						<a href="#Features">Features</a>
+					</li>
+					<li>
+						<a href="#Pricing">Pricing</a>
+					</li>
+					<li>
+						<a href="#Resources">Resources</a>
+					</li>
+					<li>
+						<a href="#Login">Login</a>
+					</li>
+					<li>
+						<a href="#SignUp">Sign Up</a>
+					</li>
+				</ul>
+			</nav>
 		</header>
 	)
 }
