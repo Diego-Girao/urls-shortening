0451a914b0b9ef040837f03d23d38d12dfb3976f
diegogirao <diegoramiro25@gmail.com>
2023-02-10 14:22:54 -0300

    feat: adicionado o hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header"
 import "./global.css"
+import { Hero } from "./Hero"
 
 export function App() {
 	return (
 		<div>
 			<Header />
+			<Hero />
 		</div>
 	)
 }
