6d72105740a61d4d2525692af170cb0d117beb1b
diegogirao <diegoramiro25@gmail.com>
2023-02-11 11:07:20 -0300

    feat: add hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { Header } from "./components/Header"
 import "./global.css"
-import { Hero } from "./Hero"
+import { Hero } from "./components/Hero"
 
 export function App() {
 	return (
 		<div>
 			<Header />
 			<Hero />
+			<h2>Sou um h2 direto no App </h2>
+			<p>Title text</p>
+			<span>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+				dignissimos, autem itaque sit facilis fuga temporibus expedita eligendi
+				quo et soluta necessitatibus repellat, in, numquam velit repellendus
+				adipisci rem similique. Lorem ipsum, dolor sit amet consectetur
+				adipisicing elit. Facere nesciunt nobis mollitia accusamus veniam
+				aperiam, hic molestiae, cupiditate incidunt amet et error aliquam maxime
+				eligendi voluptates corrupti eaque voluptatum. Quam!
+			</span>
+			<div></div>
 		</div>
 	)
 }
