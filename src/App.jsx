f94a30bff7eb3398100f0382b2f0dfed927e4669
diegogirao <diegoramiro25@gmail.com>
2023-02-12 11:24:04 -0300

    add content component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header"
 import { Hero } from "./components/Hero"
+import { Content } from "./components/Content"
 import { Footer } from "./components/Footer"
 import "./global.css"
 
@@ -8,17 +9,7 @@ export function App() {
 		<div>
 			<Header />
 			<Hero />
-			<h2>Sou um h2 direto no App </h2>
-			<p>Title text</p>
-			<span>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-				dignissimos, autem itaque sit facilis fuga temporibus expedita eligendi
-				quo et soluta necessitatibus repellat, in, numquam velit repellendus
-				adipisci rem similique. Lorem ipsum, dolor sit amet consectetur
-				adipisicing elit. Facere nesciunt nobis mollitia accusamus veniam
-				aperiam, hic molestiae, cupiditate incidunt amet et error aliquam maxime
-				eligendi voluptates corrupti eaque voluptatum. Quam!
-			</span>
+			<Content />
 			<Footer />
 		</div>
 	)
